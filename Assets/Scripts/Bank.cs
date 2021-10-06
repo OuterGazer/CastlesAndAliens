09dc967bcf81e2712b8213d89107aae8904ce371
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-10-06 14:50:05 +0200

    Implemented loose condition.

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bank : MonoBehaviour
 {
@@ -22,5 +23,10 @@ public class Bank : MonoBehaviour
     public void Withdraw(int amount)
     {
         this.currentBalance -= Mathf.Abs(amount);
+
+        if(this.currentBalance < 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
