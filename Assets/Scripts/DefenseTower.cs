71e19089a32f5adc7bbbfacc1d71c08d98e80da2
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-10-06 14:32:19 +0200

    Implemented tower cost and tower placement limitations.

---
+++ Assets/Scripts/DefenseTower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenseTower : MonoBehaviour
+{
+    [SerializeField] int goldCost = default;
+    public int GoldCost => this.goldCost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/MouseInteraction.cs
+++ Assets/Scripts/MouseInteraction.cs
@@ -9,14 +9,23 @@ public class MouseInteraction : MonoBehaviour
     [SerializeField] bool isPlaceable = default;
     public bool IsPlaceable => this.isPlaceable;
 
+    private Bank bank;
+
+    private void Start()
+    {
+        this.bank = GameObject.FindObjectOfType<Bank>();
+    }
+
     private void OnMouseDown()
     {
-        if (this.isPlaceable)
+        if (this.isPlaceable && this.bank.CurrentBalance > 0)
         {
             //Debug.Log(this.gameObject.transform.position);
             Vector3 placementPos = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + 0.2f, this.gameObject.transform.position.z);
-            Instantiate<GameObject>(this.basicTowerPrefab, placementPos, Quaternion.identity);
+            GameObject tower = Instantiate<GameObject>(this.basicTowerPrefab, placementPos, Quaternion.identity);
             this.isPlaceable = false;
+
+            this.bank.Withdraw(tower.GetComponent<DefenseTower>().GoldCost);
         }        
     }
 }
