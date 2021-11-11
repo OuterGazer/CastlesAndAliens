4c9c79ba61c1b79a4bd28115067bcff0b32553ad
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-11-11 15:21:14 +0100

    Tweaked neighbouring logic to account for adjacent tiles at different height.

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -8,9 +8,12 @@ public class Pathfinder : MonoBehaviour
     private Dictionary<Vector3Int, Node> gameGrid;
 
     [SerializeField] private Node currentSearchNode;
+    [SerializeField] Vector2Int pathStart;
+    [SerializeField] Vector2Int pathEnd;
 
     // This array influences the path depending on the order of directions
     Vector3Int[] directions = { Vector3Int.right, Vector3Int.left, Vector3Int.up, Vector3Int.down };
+    Vector3Int[] height = { Vector3Int.back, Vector3Int.forward };
 
 
     private void Awake()
@@ -39,6 +42,21 @@ public class Pathfinder : MonoBehaviour
 
             if (this.gameGrid.ContainsKey(curNeighbourCoords))
             {
+                if (GameObject.Find(curNeighbourCoords.ToString()) == null)
+                {
+                    for (int j = 0; j < this.height.Length; j++)
+                    {
+                        curNeighbourCoords = curNeighbourCoords + this.height[j];
+
+                        if (GameObject.Find(curNeighbourCoords.ToString()) != null)
+                        {
+                            break;
+                        }
+
+                        curNeighbourCoords = curNeighbourCoords - this.height[j];
+                    }
+                }                    
+
                 neighboursList.Add(this.gameGrid[curNeighbourCoords]);
 
                 // TODO: remove after testing
