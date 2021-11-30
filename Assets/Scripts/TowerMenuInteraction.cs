3b823cf33a16f2dc751d0b311ac4b1a7b0fe935d
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-11-30 14:26:08 +0100

    Implemented button text changing color with the button.

--- Assets/Scripts/TowerMenuInteraction.cs
+++ Assets/Scripts/TowerMenuInteraction.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TowerMenuInteraction : MonoBehaviour
 {
     [SerializeField] Image[] towerButtons;
+    [SerializeField] TextMeshProUGUI[] towerTexts;
 
     // Start is called before the first frame update
     void Awake()
@@ -14,12 +16,8 @@ public class TowerMenuInteraction : MonoBehaviour
         {
             item.color = Color.grey;
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SetTextsColorToGrey();
     }
 
     public void OnClick(Image towerType)
@@ -28,10 +26,29 @@ public class TowerMenuInteraction : MonoBehaviour
         {
             item.color = Color.grey;
             ChangeButtonColor(item);
+            SetTextsColorToGrey();
         }
 
         towerType.color = Color.white;
         ChangeButtonColor(towerType);
+        ChangeTextColor(towerType);
+    }
+
+    private void SetTextsColorToGrey()
+    {
+        foreach (TextMeshProUGUI item in this.towerTexts)
+        {
+            item.color = Color.grey;
+        }
+    }
+
+    private void ChangeTextColor(Image towerType)
+    {
+        for (int i = 0; i < this.towerButtons.Length; i++)
+        {
+            if (this.towerButtons[i] == towerType)
+                this.towerTexts[i].color = Color.white;
+        }
     }
 
     private void ChangeButtonColor(Image buttonImage)
